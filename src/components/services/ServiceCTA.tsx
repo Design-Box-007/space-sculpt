d7a030aae80bb736287bba8cc4c60b9d1cacd541
Akram-00 <aliakram9789@gmail.com>
2025-02-28 23:08:35 +0530

    service CTA is updated

--- src/components/services/ServiceCTA.tsx
+++ src/components/services/ServiceCTA.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const ServiceCTA = () => {
@@ -12,14 +13,16 @@ const ServiceCTA = () => {
                     We bring creativity, precision, and craftsmanship together to transform your spaces into personalized works of art.
                 </p>
 
-                <button className="flex gap-2 mt-10 w-full bg-white flex-row-reverse items-center justify-between py-4 sm:py-6 px-6 sm:px-10 rounded-full">
-                    <span className="bg-primary rounded-full p-2 sm:p-3">
-                        <ArrowRight size={40} className="text-white -rotate-45" />
-                    </span>
-                    <span className="text-primary text-xl sm:text-3xl md:text-4xl lg:text-5xl font-medium">
-                        Get Free Consultation
-                    </span>
-                </button>
+                <Link href={'/contact'} id='contact'>
+                    <button className="flex gap-2 mt-10 w-full bg-white flex-row-reverse items-center justify-between py-4 sm:py-6 px-6 sm:px-10 rounded-full">
+                        <span className="bg-primary rounded-full p-2 sm:p-3">
+                            <ArrowRight size={40} className="text-white -rotate-45" />
+                        </span>
+                        <span className="text-primary text-xl sm:text-3xl md:text-4xl lg:text-5xl font-medium">
+                            Get Free Consultation
+                        </span>
+                    </button>
+                </Link>
             </div>
         </section>
     )

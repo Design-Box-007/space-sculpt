c6a7fb1206c61d6aa7a78327b13c47f7424146a2
Akram-00 <aliakram9789@gmail.com>
2025-06-04 02:35:55 +0530

    Added metadata to services

--- src/app/services/[serviceName]/page.tsx
+++ src/app/services/[serviceName]/page.tsx
@@ -1,7 +1,58 @@
 import Service from '@/components/service/Service'
+import services from '@/data/services';
+import formatToHyphenated from '@/utils/formatPathName';
 import React from 'react'
 
+export const generateMetadata = async ({ params }: { params: Promise<{ serviceName: string }> }) => {
+    // Awaiting the params to resolve the Promise
+    const { serviceName } = await params;
+
+    const serviceData = services.find(service =>
+        serviceName === formatToHyphenated(service.title)
+    );
+
+    if (!serviceData) {
+        return {
+            title: "service Not Found | Space Sculpt service",
+            description: "We couldn't find the service you're looking for.",
+            metadataBase: new URL("https://spacesculpt.ae"),  // Set metadataBase for external URLs
+        };
+    }
+
+    return {
+        title: `${serviceData.metatags.title} | Space Sculpt service`,
+        description: serviceData.metatags.description,
+        alternates: {
+            canonical: `https://spacesculpt.ae/services/${serviceName}`,
+        },
+        openGraph: {
+            title: serviceData.title,
+            description: serviceData.description,
+            url: `https://spacesculpt.ae/services/${serviceName}`,
+            images: [
+                {
+                    url: serviceData.imgSrc || 'https://spacesculpt.ae/default-og-image.jpg',
+                    alt: serviceData.title,
+                }
+            ],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: serviceData.metatags.title,
+            description: serviceData.metatags.description,
+            images: [serviceData.imgSrc || 'https://spacesculpt.ae/default-og-image.jpg'],
+        },
+        robots: {
+            index: true,
+            follow: true,
+        },
+        metadataBase: new URL("https://spacesculpt.ae"),  // Set metadataBase for external URLs
+    };
+};
+
+
 const page = () => {
+
     return (
         <Service />
     )

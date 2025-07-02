3bfb2d2d550962199a6de7dc576238bc1b34e173
Akram-00 <aliakram9789@gmail.com>
2025-07-02 12:36:29 +0530

    changed to es6 imports

--- src/scripts/sitemap-generator.js
+++ src/scripts/sitemap-generator.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 // Base URL (update as needed)
 const BASE_URL = 'https://www.spacesculpt.ae';
